12e94456bd1124f05904a7e4f2e1a881f91f386e
Sammy Hubner <me@sammy.nu>
2023-10-27 00:02:02 +0200

    add vimfiles as git submodule

--- aliases.zsh
+++ aliases.zsh
@@ -2,10 +2,12 @@
 alias zshreload='source ~/.zshrc'
 alias zshconfig='vi ~/Dotfiles/zshrc'
 alias zshalias='vi ~/Dotfiles/aliases.zsh'
+alias vimconfig="vi ~/Dotfiles/vimfiles/vimrc"
 
 # Navigation
 alias cc='cd ~/Code'
 alias dotfiles='cd ~/Dotfiles'
+alias vimfiles='cd ~/Dotfiles/vimfiles'
 
 # Git
 alias g=git
@@ -17,5 +19,17 @@ alias ga='g add'
 alias gap='ga -p'
 alias gau='ga -u'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gb="g branch"
+alias gd="g diff"
+alias gdc="gd --cached"
 
+# Docker
+alias d=docker
+alias dc=docker-compose
 
+# Utils
+alias ll="ls -lah"
+
+# npm
+alias nis="npm install --save"
+alias nid="npm install --save-dev"
